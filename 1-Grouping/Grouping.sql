af5cd7b6797c5086778ad1a2e7a5589daa9f53de
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-11-29 21:06:02 -0300

    Gruping of datas

---
+++ 1-Grouping/Grouping.sql
@@ -0,0 +1,24 @@
+/*
+INSERTS FOR CATEGORIES:
+INSERT INTO categories VALUES (null, 'Action');
+INSERT INTO categories VALUES (null, 'Sport');
+INSERT INTO categories VALUES (null, 'Rol');
+
+
+
+
+
+INSERTS FOR ENTERS
+INSERT INTO enters VALUES (null, '1', '1', 'GTA 5 News', 'GTA 5 Review', CURDATE());
+INSERT INTO enters VALUES (null, '1', '2', 'LOL News', 'All about LOL', CURDATE());
+INSERT INTO enters VALUES (null, '1', '3', 'FIFA News', 'News Players for FIFA 2022', CURDATE());
+
+INSERT INTO enters VALUES (null, '2', '1', 'Assasins', 'Assasins Review', CURDATE());
+INSERT INTO enters VALUES (null, '2', '2', 'WOW News', 'All about WOW', CURDATE());
+INSERT INTO enters VALUES (null, '2', '3', 'PES News', 'News Players for PES 2022', CURDATE());
+
+INSERT INTO enters VALUES (null, '3', '1', 'Call of Duty News', 'Call of Duty Review', CURDATE());
+INSERT INTO enters VALUES (null, '3', '2', 'Fornite News', 'All about Fornite', CURDATE());
+INSERT INTO enters VALUES (null, '3', '3', 'F1 2022 News', 'News Players for F1 2022 2022', CURDATE());
+
+
