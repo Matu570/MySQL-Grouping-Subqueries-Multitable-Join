b5a78cce70ab19220d5ca3cc75237c29232b1cff
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-11-30 23:36:45 -0300

    Exercise 2 Subqueries

--- 4-Subqueries/Subqueries.sql
+++ 4-Subqueries/Subqueries.sql
@@ -1,5 +1,17 @@
 SELECT * FROM users;
+
+-- Excersise 1:
+-- Get to all users with and with out enters.
+
 INSERT INTO users VALUES(NULL, 'Name', 'Example', 'example@email', '4321', CURDATE());
 SELECT * FROM users WHERE id_user IN (SELECT user_id FROM enters);
 
 SELECT * FROM users WHERE id_user NOT IN (SELECT user_id FROM enters);
+
+-- Excersise 2:
+-- Get to all users with enters about GTA.
+
+SELECT * FROM enters;
+INSERT INTO enters VALUES(NULL, 3, 1, "GTA Guide", "GTA", CURDATE());
+SELECT name, last_name FROM users WHERE id_user IN (SELECT user_id FROM enters WHERE title LIKE "%GTA%");
+
